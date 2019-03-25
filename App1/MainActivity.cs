c84eefda75ea951e09e5e6de9fe078f5d37eb644
xweronika <xweronika19@gmail.com>
2019-03-25 22:35:33 +0100

    init

---
+++ App1/MainActivity.cs
@@ -0,0 +1,30 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+
+namespace App1
+{
+    [Activity(Label = "Aplikacja", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        TextView txtNumber;
+        int number;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            txtNumber = FindViewById<TextView>(Resource.Id.txtNumber);
+
+            FindViewById<Button>(Resource.Id.button1).Click += (o, e) =>
+            txtNumber.Text = (++number).ToString();
+
+            FindViewById<Button>(Resource.Id.button2).Click += (o, e) =>
+            txtNumber.Text = (--number).ToString();
+        }
+    }
+}
